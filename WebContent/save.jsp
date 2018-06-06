dfb2038740260566af310ff6f5c1ede3237c4e88
kenl6 <ken727800@163.com>
2018-06-06 11:43:54 +0800

    增加jstl模板

--- WebContent/save.jsp
+++ WebContent/save.jsp
@@ -1,19 +1,20 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
-    pageEncoding="UTF-8"%>
+	pageEncoding="UTF-8"%>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
 <title>添加商品</title>
+<%@ include file="/WEB-INF/public.jspf" %>>
 </head>
 <body>
 	<!-- action就是配置提交到后台的地址，请注意在web项目中，所有的请求都需要添加工程名 -->
 	<!-- *.mvc在web.xml中已经配置，只有Mvc后缀的请求才会交给Spring-mvc处理 -->
 	<!-- request.getContextPath() 动态获取当前项目工程名 -->
-	<form action="<%=request.getContextPath() %>/product/save.mvc" method="post">
-		商品名称：<input type="text" name="name" /><br/>
-		商品价格：<input type="text" name="price" /><br/>
-		商品备注：<input type="text" name="remark" /><br/>
-		<button type="submit" >保存</button>
+	<form action="<%=request.getContextPath()%>/product/save.mvc"
+		method="post">
+		商品名称：<input type="text" name="name" /><br /> 商品价格：<input type="text"
+			name="price" /><br /> 商品备注：<input type="text" name="remark" /><br />
+		<button type="submit">保存</button>
 	</form>
 </body>
 </html>
--- src/cn/dw/controller/ProductController.java
+++ src/cn/dw/controller/ProductController.java
@@ -1,6 +1,11 @@
 package cn.dw.controller;
 
+import java.util.List;
+
 import javax.annotation.Resource;
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,16 +19,29 @@ import cn.dw.service.ProductServiceImpl;
  */
 @RequestMapping(value = "/product")
 @Controller // @Controller注解代表当前类交给Spring
-public class ProductController {
+public class ProductController extends BaseController {
+	
+	
 	// MVC推荐使用注解，因此此处采用注解取代XML
 	@Resource(name = "productService") // Resource会到配置文件中查找ID为指定名称的bean
 	private ProductServiceImpl productService;
 
 	@RequestMapping(value = "/save")
-	public void save(Product product) {
+	public String save(Product product) {
 		// 1、此处应该调用ProductService.save();
 		productService.save(product);
 		// 2、跳转到查询页面
+		return	"redirect:/query.jsp";
+
+	}
+	
+	@RequestMapping(value = "/queryByName")
+	public String queryByName(String keyword) {
+		// 1、此处应该调用ProductService.queryByName();
+		List<Product> proList = productService.queryByName("%"+keyword+"%", 1, 10);
+		request.setAttribute("proList", proList);
+		// 2、跳转到查询页面
+		return	"forward:/query.jsp";
 
 	}
 }
